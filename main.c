e98e1eedd9cb56a596bb078e1b1db14bb1767b6c
Oumar Mimouni <oumar.mimouni@gmail.com>
2021-07-13 13:20:03 +0100

    clean up

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 01:54:48 by omimouni          #+#    #+#             */
-/*   Updated: 2021/07/13 13:16:52 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/07/13 13:19:10 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	stack_print(t_stack	**stack)
+static void	init(t_stack	**stack)
 {
 	double	m;
 
@@ -27,12 +27,9 @@ int	main(int argc, char **argv)
 
 	a = ps_stack_new();
 	b = ps_stack_new();
-	// Push to stack A
 	while (argc > 1)
 		ps_stack_push(&a, (double)atoi(argv[--argc]));
 	ps_action_rrotate(&a);
-
-	// Print stacks at the end
 	printf("Stack a (%ld):\n", a->size);
 	stack_print(&a);
 	printf("\nStack b (%ld):\n", b->size);
