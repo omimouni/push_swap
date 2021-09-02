7260170c5fabc35d5952f796e5e0679b6ea870b1
Oumar Mimouni <oumar.mimouni@gmail.com>
2021-09-02 11:50:14 +0100

    sort small numbers

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -6,17 +6,14 @@
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 11:22:16 by omimouni          #+#    #+#             */
-/*   Updated: 2021/09/02 11:42:19 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/09/02 11:48:41 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ps_sort_small(t_data *dt)
+static void	ps_sort_small_3(t_data *dt, double *list)
 {
-	double	*list;
-
-	list = dt->stack_a.list;
 	if (list[0] > list[1] && list[1] < list[2] && list[2] < list[0])
 		ps_exec("sa", dt);
 	else if (list[0] < list[1] && list[1] < list[2] && list[2] > list[0])
@@ -33,4 +30,19 @@ void	ps_sort_small(t_data *dt)
 	}
 	else if (list[0] < list[1] && list[1] > list[2] && list[2] > list[0])
 		ps_exec("rra", dt);
+}
+
+void	ps_sort_small(t_data *dt)
+{
+	if (dt->length == 3)
+		ps_sort_small_3(dt, dt->stack_a.list);
+	if (dt->length == 5)
+	{
+		ps_exec("pb", dt);
+		ps_exec("pb", dt);
+		ps_sort_small_3(dt, dt->stack_a.list);
+		ps_exec("pa", dt);
+		ps_exec("ra", dt);
+		ps_exec("pa", dt);
+	}
 }
